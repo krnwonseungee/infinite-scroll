090e6dcc028f812bc827f38417671900c3f38402
Janet Yi <janetyi90@gmail.com>
2014-09-07 15:27:40 -0700

    Adds comments, creates pre-made quote list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function(){
+  //creates Quote model w/default attributes
   var Quote = Backbone.Model.extend({
     defaults: {
       text: '',
@@ -7,9 +8,19 @@ $(function(){
 
   });
 
+//creates Quotes collection, holds objects of Quote model
   var QuoteList = Backbone.Collection.extend({
     model: Quote
 
   });
+
+  // creates pre-made collection of quotes
+  var quotes = new QuoteList([
+    new Quote({ author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken.' }),
+    new Quote({ author: 'Mahatma Gandhi', text: 'Be the change that you wish to see in the world.' }),
+    new Quote({ author: 'Eleanor Roosevelt', text: 'No one can make you feel inferior without your consent.' }),
+    new Quote({ author: 'Martin Luther King, Jr.', text: 'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.' }),
+    new Quote({ author: 'Marilyn Monroe', text: 'Imperfection is beauty, madness is genius and it\'s better to be absolutely ridiculous than absolutely boring.' }),
+  ]);
 
 });
